2e01d7ff367d0752c37419ccc80985a41f8a9f8e
Chris Albrecht <vegashat@gmail.com>
2012-09-23 15:31:24 -0500

    commit before win8 install

---
+++ Bearchop/Bearchop/Mailers/IUserMailer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mail;
+using Contests.LOTW.Core.Model;
+
+namespace Bearchop.Mailers
+{
+    public interface IUserMailer
+    {
+        MailMessage MailCurrentPicks(Week week, IList<Pick> picks);
+    }
+}
---
+++ Bearchop/Bearchop/Mailers/UserMailer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mail;
+using Contests.LOTW.Core.Model;
+using Mvc.Mailer;
+
+namespace Bearchop.Mailers
+{
+    public class UserMailer : MailerBase, IUserMailer
+    {
+        public UserMailer() :
+            base()
+        {
+            MasterName = "_Layout";
+
+        }
+
+        public MailMessage MailCurrentPicks(string email, Week week, IList<Pick> picks)
+        {
+            var mailMessage = new MailMessage { Subject = "Picks for LOTW : ".+ week.Number.ToString() };
+            mailMessage.To = email;
+
+
+            ViewData.Model = picks;
+            PopulateBody(mailMessage, viewName: "NewLineRequestWriting");
+            return mailMessage;
+        }
+
+
+    }
+}
