0f6b25df498dd62e86ffd3101ced9f57d950dcd2
Chris Albrecht <chris.albrecht@hobbylobby.com>
2014-09-03 11:47:50 -0500

    add nfl line download

---
+++ NFLLineUpdater/Program.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NFLLineUpdater
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var uri = new Uri(@"https://www.kimonolabs.com/api/cvpz3e9q?apikey=LSzFj75rKymrmzBYruhZ4z2xU8HOh9nQ");
+
+            string fileName = string.Format("nfllines_{0}.json", DateTime.Today.ToString("MMddyyyy"));
+
+            new WebClient().DownloadFile(uri, fileName);
+
+        
+            var data = File.ReadAllText(fileName);
+            var week = (Week)(JsonConvert.DeserializeObject(data, typeof(Week)));
+        }
+    }
+
+
+    public class Week
+    {
+        public int Count { get; set; }
+
+        public Result Results { get; set; }
+    }
+
+    public class Result
+    {
+        public List<Game> Week { get; set; }
+    }
+
+    public class  Game
+    {
+        public string AwayTeam { get; set; }
+        public string HomeTeam { get; set; }
+        public string OverUnder { get; set; }
+        public string HomeTeamSpread { get; set; }
+    }
+
+    
+}
